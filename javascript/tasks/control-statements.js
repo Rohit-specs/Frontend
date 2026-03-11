499ab50a5d6d0a94f89a159776c8ccb4b33d1914
rohit-specs <rohit5592006@gmail.com>
2026-03-11 14:52:20 +0530

    control statement task

---
+++ javascript/tasks/control-statements.js
@@ -0,0 +1,14 @@
+for (let i = 2; i <= 50; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i)
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 == 0) {
+        console.log(i)
+        console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+        console.log(i)
+        console.log("Buzz");
+    }
+}
