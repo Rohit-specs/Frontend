c226c21a92a18c0801c5e432b7bcf1f0c67bc7e0
rohit-specs <rohit5592006@gmail.com>
2026-03-13 19:38:08 +0530

    updated

--- javascript/tasks/datetime.js
+++ javascript/tasks/datetime.js
@@ -5,7 +5,6 @@ days_list = ["Sunday","Monday","Tuesday","Wednesday","Thusday","Friday","Saturda
 let year = current_datetime.getFullYear()
 let month = current_datetime.getMonth()
 let date = current_datetime.getDate()
-// let hour = ((current_datetime.getHours()+1)/2<=12)?`${current_datetime.getHours()+1}`:`${(((current_datetime.getHours())/2)-1)}`;
 let hour;
 if (!(current_datetime.getHours()%12)){
     hour = 12
@@ -15,7 +14,7 @@ else{
 }
 let minutes = current_datetime.getMinutes()
 let seconds = current_datetime.getSeconds()
-let meridiem = ((current_datetime.getHours()+1)/2<=12)?"PM":"AM";
+let meridiem = ((current_datetime.getHours()+1)<=12)?"AM":"PM";
 let day = current_datetime.getDay()
 let current_date = `Date: ${date}/${months_list[month]}/${year}`;
 let current_time = `Time: ${hour}:${minutes}:${seconds} ${meridiem}`
