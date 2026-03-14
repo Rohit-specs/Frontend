62d015d128f2bf9f54c9b3f38cba223121140a6d
rohit-specs <rohit5592006@gmail.com>
2026-03-15 00:05:44 +0530

    added string slicing

--- javascript/string methods/main.js
+++ javascript/string methods/main.js
@@ -20,3 +20,24 @@ let phoneNumber = "123-456-7890";
 console.log(phoneNumber.replaceAll("-",""));
 console.log(phoneNumber.padStart("15","0"));
 console.log(phoneNumber.padEnd("15","0"));
+
+
+// string slicing = creating a substring from a portion of another string
+                    // string.slice(start,end)
+
+const fullName = "Rohit Kumar"
+// let firstName = fullName.slice(0,5)
+// let lastName = fullName.slice(6,11)
+let firstName = fullName.slice(0,fullName.indexOf(" "))
+let lastName = fullName.slice(fullName.indexOf(" ")+1,)
+console.log(firstName);
+console.log(lastName);
+let firstChar = fullName.slice(0,1)
+let lastChar = fullName.slice(-1)
+console.log(firstChar);
+console.log(lastChar);
+const email = "rohit@gmail.com";
+let Username = email.slice(0,email.indexOf("@"));
+let extension = email.slice(email.indexOf("@"));
+console.log(Username);
+console.log(extension);
