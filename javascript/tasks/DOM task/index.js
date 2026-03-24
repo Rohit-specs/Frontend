371eb049c871d15bb0433338105b97ec5473e8c9
rohit-specs <rohit5592006@gmail.com>
2026-03-24 12:24:51 +0530

    task updated

--- javascript/tasks/DOM task/index.js
+++ javascript/tasks/DOM task/index.js
@@ -7,40 +7,35 @@ const users = [
 
 // users.sort()
 
-  let activeCount = 0;
-  let inactiveCount = 0;
+let activeCount = 0;
+let inactiveCount = 0;
+
+const tableBody = document.getElementById("tabledata");
+users.forEach(user => {
+  const tr = document.createElement("tr");
+  if (user.active) {
+    activeCount++
+  } else {
+    inactiveCount++
+  }
+  const tdId =  document.createElement("td");
+  tdId.textContent = user.id
   
-  const tableBody = document.getElementById("tabledata");
-  users.forEach(user => {
-    const tr = document.createElement("tr");
+  const tdname =  document.createElement("td");
+  tdname.textContent = user.name
+  const tdrole =  document.createElement("td");
+  tdrole.textContent = user.role
+  const tdactive =  document.createElement("td");
+  tdactive.textContent = user.active ? "Active" : "Inactive";
+  tr.appendChild(tdId) 
+  tr.appendChild(tdname) 
+  tr.appendChild(tdrole)
+  tr.appendChild(tdactive)
 
-    if (user.active) {
-      activeCount++;
-    } else {
-      inactiveCount++;
-    }
-
-
-    const tdId = document.createElement("td");
-    tdId.textContent = user.id;
-
-    const tdName = document.createElement("td");
-    tdName.textContent = user.name;
-
-    const tdRole = document.createElement("td");
-    tdRole.textContent = user.role;
-
-    const tdActive = document.createElement("td");
-
-    tdActive.textContent = user.active ? "active" : "inactive";
-
-    tr.appendChild(tdId);
-    tr.appendChild(tdName);
-    tr.appendChild(tdRole);
-    tr.appendChild(tdActive);
+  tableBody.appendChild(tr)
   
-    tableBody.appendChild(tr);
-  });
+});
 
-
-
+const summary = document.getElementById("summary")
+let summaryContent = `Total Users: ${users.length} | Active: ${activeCount} | Inactive: ${inactiveCount}`
+summary.textContent = summaryContent
