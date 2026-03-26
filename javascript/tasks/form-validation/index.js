75fa48d3faae133156ed016ae55a45f9fcd039a2
rohit-specs <rohit5592006@gmail.com>
2026-03-26 10:34:03 +0530

    form validation task completed

--- javascript/tasks/form-validation/index.js
+++ javascript/tasks/form-validation/index.js
@@ -1,4 +1,4 @@
-document.getElementById("form").addEventListener("submit",function (e){
+document.getElementById("form").addEventListener("submit", function (e) {
     e.preventDefault();
     let name = document.getElementById("name")?.value;
     let email = document.getElementById("email")?.value;
@@ -8,68 +8,98 @@ document.getElementById("form").addEventListener("submit",function (e){
 
     let isValid = true;
 
-    if(!validName(name)){
+    if (!validName(name)) {
         isValid = false
         let feedBack = document.getElementById("name").nextElementSibling;
         feedBack.textContent = "Atleast 3 character in first name"
     }
-    else{
+    else {
         let feedBack = document.getElementById("name").nextElementSibling;
         feedBack.textContent = ""
     }
-    if(!validEmail(email)){
+    if (!validEmail(email)) {
         isValid = false
         let feedBack = document.getElementById("email").nextElementSibling;
         feedBack.textContent = "Invalid email"
     }
-    else{
+    else {
         let feedBack = document.getElementById("email").nextElementSibling;
         feedBack.textContent = ""
     }
-    if(!validPhone(phone)){
+    if (!validPhone(phone)) {
         isValid = false
         let feedBack = document.getElementById("phone").nextElementSibling;
         feedBack.textContent = "Invalid Mobile number"
     }
-    else{
+    else {
         let feedBack = document.getElementById("phone").nextElementSibling;
         feedBack.textContent = ""
     }
-    if(!validPassword(password)){
+    if (!validPassword(password)) {
         isValid = false
         let feedBack = document.getElementById("password").nextElementSibling;
         feedBack.textContent = "Invalid password"
     }
-    else{
+    else {
         let feedBack = document.getElementById("password").nextElementSibling;
         feedBack.textContent = ""
     }
-    if(!validPassword(confirmPassword)){
+    if (!validPassword(confirmPassword)) {
         isValid = false
         let feedBack = document.getElementById("confirmPassword").nextElementSibling;
         feedBack.textContent = "Invalid Password"
     }
-    else if(password!=confirmPassword){
+    else if (password != confirmPassword) {
         let feedBack = document.getElementById("confirmPassword").nextElementSibling;
         feedBack.textContent = "Password didn't match"
     }
-    else{
+    else {
         let feedBack = document.getElementById("confirmPassword").nextElementSibling;
         feedBack.textContent = ""
     }
     let skills = document.querySelectorAll(".check-box-group input[name='skill']:checked")
-    let skillsArray = Array.from(skills).map((element)=>element.value)
+    let skillsArray = Array.from(skills).map((element) => element.value)
     let skillLength = skillsArray.length
 
-    if(skillLength<2){
+    if (skillLength < 2) {
         isValid = false;
         let feedBack = document.querySelector(".check-box-group #errorFeedback")
         feedBack.textContent = "Have to select atleast two skills"
     }
-    else{
+    else {
         let feedBack = document.querySelector(".check-box-group #errorFeedback")
         feedBack.textContent = ""
     }
+    genders = document.querySelectorAll("input[name='gender']")
+
+    let isChecked = false;
+    genders.forEach(genders => {
+        if(genders.checked){
+            isChecked = true;
+        }
+    });
+    if(!isChecked){
+        isValid = false;
+        let feedBack = document.querySelector(".radio-group #errorFeedback");
+        feedBack.textContent = "Please select gender before submit"
+    }
+    else{
+        let feedBack = document.querySelector(".radio-group #errorFeedback");
+        feedBack.textContent = ""
+    }
+
+    // genders.forEach(gender => {
+    //     if (gender.checked) {
+    //         isValid = false
+    //         not_checked
+    //         let feedBack = document.querySelector(".radio-group .errorFeedback");
+    //         feedBack.textContent = "Please select gender before submit"
+    //     }
+    //     else{
+    //         let feedBack = document.querySelector(".radio-group .errorFeedback");
+    //         feedBack.textContent = ""
+    //     }
+    // });
 
     if (isValid) {
         window.alert("Registration Successful")
@@ -79,21 +109,21 @@ document.getElementById("form").addEventListener("submit",function (e){
 function validName(name) {
     name = name.trim();
     let spaceregex = /\s+/g
-    name = name.replace(spaceregex," ");
+    name = name.replace(spaceregex, " ");
     let nameregex = /^[a-zA-ZÀ-ÿ]{3,}(?: [a-zA-ZÀ-ÿ]+)+$/
     return nameregex.test(name);
 }
-function validEmail(email){
+function validEmail(email) {
     email = email.trim()
     let emailregex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     return emailregex.test(email);
 }
-function validPhone(phone){
+function validPhone(phone) {
     phone = phone.trim()
     let phoneregex = /^(?:(?:\+|0{0,2})91[\s-]?|0)?[6-9]\d{9}$/
     return phoneregex.test(phone);
 }
-function validPassword(password){
+function validPassword(password) {
     password = password.trim()
     let passwordregex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
     return passwordregex.test(password);
